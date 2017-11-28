ef3e8a37a7b96784a81a4f8019ddb3e03899376f
K <zander0717@gmail.com>
2017-11-28 21:03:10 +0900

    data

--- js/vendor/foundation.js
+++ js/vendor/foundation.js
@@ -11724,7 +11724,7 @@ __WEBPACK_IMPORTED_MODULE_1__home_deployer_sites_node_foundation_customizer_node
 /***/ })
 /******/ ]);
 
-var headerHight = $("sticky-container").outerHeight();
+var headerHight = $("data-sticky-container").outerHeight();
 $("a").click(function() {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "body" : href);
