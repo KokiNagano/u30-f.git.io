d225cdba6ae330b819969e64154bedf155eea2ea
K <zander0717@gmail.com>
2017-12-03 18:45:30 +0900

    add.js

--- js/add.js
+++ js/add.js
@@ -1,11 +1,3 @@
-$("#fakeloader").fakeLoader({
-        timeToHide:1200, //アイコンが表示される時間(ミリ秒)
-        zIndex:"999",//zIndexの値
-        spinner:"spinner1",//ローディングアイコンの種類'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
-        bgColor:"#2c3840", //RGBもしくはRGBAの16進数の値
-        //imagePath:"yourPath/customizedImage.gif" //オリジナル画像を使用する場合の画像へのパス
-        });
-
 $(document).on('click', '.open-options', function(event) {
   event.preventDefault();
   $('#modal-options').iziModal('open');
@@ -17,4 +9,30 @@ $('#modal-options').iziModal({
   overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
   transitionIn: 'fadeInUp', //表示される時のアニメーション
   transitionOut: 'fadeOutDown' //非表示になる時のアニメーション
+});
+
+$(document).on('click', '.open-options2', function(event) {
+  event.preventDefault();
+  $('#modal-options2').iziModal('open');
+});
+$('#modal-options2').iziModal({
+  headerColor: '#26A69A', //ヘッダー部分の色
+  width: 400, //横幅
+  overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
+  transitionIn: 'fadeInUp', //表示される時のアニメーション
+  transitionOut: 'fadeOutDown', //非表示になる時のアニメーション
+  padding: 20
+});
+
+$(document).on('click', '.open-options3', function(event) {
+  event.preventDefault();
+  $('#modal-options3').iziModal('open');
+});
+$('#modal-options3').iziModal({
+  headerColor: '#26A69A', //ヘッダー部分の色
+  width: 400, //横幅
+  overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
+  transitionIn: 'fadeInUp', //表示される時のアニメーション
+  transitionOut: 'fadeOutDown', //非表示になる時のアニメーション
+  padding: 20
 });
